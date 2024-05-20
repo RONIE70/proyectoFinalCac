f3e04c3c9f72cf91a53c1910f6d80d22c9287e3a
RONIE70 <paularonie70@gmail.com>
2024-05-20 15:59:05 -0300

    consumo API

--- js/json.js
+++ js/json.js
@@ -1,37 +1,32 @@
+document.getElementById("miFormulario").addEventListener("submit", function (event) {
+    event.preventDefault();
 
-        document.getElementById("miFormulario").addEventListener("submit",function(event)
-        {
-            event.preventDefault();
+    var nombre = document.getElementById("nombre").value;
+    var apellido = document.getElementById("apellido").value;
+    var email = document.getElementById("email").value;
+    var nombreError = document.getElementById("nombreError");
+    var apellidoError = document.getElementById("apellidoError");
+    var emailError = document.getElementById("emailError");
+    var regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-            var nombre=document.getElementById("nombre").value;
-            var apellido=document.getElementById("apellido").value;
-            var email=document.getElementById("email").value;
-            var nombreError=document.getElementById("nombreError");
-            var apellidoError=document.getElementById("apellidoError");
-            var emailError=document.getElementById("emailError");
-            var regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    nombreError.textContent = "";
+    apellidoError.textContent = "";
+    emailError.textContent = "";
 
-            nombreError.textContent="";
-            apellidoError.textContent="";
-            emailError.textContent="";
-        
-            if(nombre.trim()==="")
-            {
-                nombreError.textContent="Complete el campo con su nombre";
-                return;
-            }
+    if (nombre.trim() === "") {
+        nombreError.textContent = "Complete el campo con su nombre";
+        return;
+    }
 
-            if(apellido.trim()==="")
-            {
-                apellidoError.textContent="Complete el campo con su apellido";
-                return;
-            }
+    if (apellido.trim() === "") {
+        apellidoError.textContent = "Complete el campo con su apellido";
+        return;
+    }
 
-            if(!regexEmail.test(email))
-            {
-                emailError.textContent="No has ingresado un mail";
-                return ;
-            }
-            alert("EL formulario se envio con exito");
-        });
-   
+    if (!regexEmail.test(email)) {
+        emailError.textContent = "No has ingresado un mail";
+        return;
+    }
+    alert("EL formulario se envio con exito");
+});
+
